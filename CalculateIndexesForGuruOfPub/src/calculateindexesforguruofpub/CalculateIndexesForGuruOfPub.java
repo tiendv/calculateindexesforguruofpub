3fefbb439da188691daab320f066c294a170ac46
tranhungnghiep@gmail.com <tranhungnghiep@gmail.com@0b7a0610-f192-9a71-9011-bcf9fa636569>
2011-12-01 16:28:14 +0000

    Update code.

--- CalculateIndexesForGuruOfPub/src/calculateindexesforguruofpub/CalculateIndexesForGuruOfPub.java
+++ CalculateIndexesForGuruOfPub/src/calculateindexesforguruofpub/CalculateIndexesForGuruOfPub.java
@@ -4,6 +4,9 @@
  */
 package calculateindexesforguruofpub;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import uit.guruofpub.utility.CalculateIndex;
 import uit.guruofpub.utility.GuruOfPubLogger;
 /**
@@ -16,15 +19,32 @@ public class CalculateIndexesForGuruOfPub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Start Running");
+        System.out.println("**********************************************************");
+        System.out.println("BEGIN CALCULATING");
+        System.out.println("**********************************************************");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Calendar cal = Calendar.getInstance();
+        System.out.println(dateFormat.format(cal.getTime()));
+        GuruOfPubLogger.logger.warning("**********************************************************");
+        GuruOfPubLogger.logger.warning("BEGIN CALCULATING");
+        GuruOfPubLogger.logger.warning("**********************************************************");
+        GuruOfPubLogger.logger.warning(dateFormat.format(cal.getTime()));
+
         try {
             CalculateIndex calculateIndex = new CalculateIndex();
-            System.out.println("Start calculating index for author.");
+
+            System.out.println("Begin calculating index for author.");
+            GuruOfPubLogger.logger.warning("Begin calculating index for author.");
             calculateIndex.calculateIndexAuthor();
             System.out.println("End calculating index for author.");
-            System.out.println("Start calculating index for org.");
+            GuruOfPubLogger.logger.warning("End calculating index for author.");
+            
+            System.out.println("Begin calculating index for org.");
+            GuruOfPubLogger.logger.warning("Begin calculating index for org.");
             calculateIndex.calculateIndexOrg();
             System.out.println("End calculating index for org.");
+            GuruOfPubLogger.logger.warning("End calculating index for org.");
+            
         } catch (Exception ex) {
             ex.printStackTrace();
             GuruOfPubLogger.logger.severe("EXCEPTION: " + ex.toString());
@@ -33,6 +53,15 @@ public class CalculateIndexesForGuruOfPub {
                 for (Object stackTraceElement : arrObj)
                     GuruOfPubLogger.logger.severe("\tat " + stackTraceElement.toString());
         }
-        System.out.println("End Running");
+
+        cal = Calendar.getInstance();
+        System.out.println(dateFormat.format(cal.getTime()));
+        System.out.println("**********************************************************");
+        System.out.println("END CALCULATING");
+        System.out.println("**********************************************************");
+        GuruOfPubLogger.logger.warning(dateFormat.format(cal.getTime()));
+        GuruOfPubLogger.logger.warning("**********************************************************");
+        GuruOfPubLogger.logger.warning("END CALCULATING");
+        GuruOfPubLogger.logger.warning("**********************************************************");
     }
 }
